5ceac708c98c03617eec32779bd0ae7d8e7da6d7
pankaj6436 <pankajvaidya302@gmail.com>
2026-02-04 15:41:49 +0530

    Add welcome message for DevOps day 4

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("welcome to Devops day 2 session");
 		System.out.println("welcome to Devops day 3 session");
+		System.out.println("welcome to Devops day 4 session");
 	}
 }
 
