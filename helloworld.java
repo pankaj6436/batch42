3086f708c51d1e5eb23f9e141146029e0e679e5a
pankaj6436 <pankajvaidya302@gmail.com>
2026-02-04 15:59:42 +0530

    Add welcome message for DevOps day 5
    code is completed my end

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,7 @@ public class helloworld {
 		System.out.println("welcome to Devops day 2 session");
 		System.out.println("welcome to Devops day 3 session");
 		System.out.println("welcome to Devops day 4 session");
+		System.out.println("welcome to Devops day 5 session");
 	}
 }
 
